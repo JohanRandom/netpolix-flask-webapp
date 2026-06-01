a4130bbc1bc25c3267e14f6a6c21dba9fb08892b
Johan Moreno <janmoreno@poligran.edu.co>
2026-06-01 17:24:00 -0500

    chore: exclude environment variables

--- conexion.py
+++ conexion.py
@@ -4,11 +4,23 @@
 # utilizando SQLAlchemy como ORM.
 # ---------------------------------------------------------
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 # Cadena de conexión a MySQL
-engine = create_engine("mysql+mysqlconnector://root:1234@localhost/netpolix")
+engine = create_engine(
+    f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+)
 
 # Session es la fábrica de sesiones
 Session = sessionmaker(bind=engine)
